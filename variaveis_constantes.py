e7a5982ece110cd05ea031987bbea82df152af07
EstherJDorneles <estherjdorneles@gmail.com>
2024-04-10 22:27:22 -0300

    variaveis e constantes

---
+++ variaveis_constantes.py
@@ -0,0 +1,15 @@
+name = "Maria"
+age = 30
+
+name = "Andre"
+age = 25
+print(name,age)
+
+limite_saque_diario = 1500
+
+name, age = "Angelo", 90
+
+print(name, age)
+
+BRAZIL_STATES = ["SP", "RS", "RJ", "SC", "BH"]
+print(BRAZIL_STATES)
